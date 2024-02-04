da2d523c07d083c93e2af76ad93ecf2736e683d5
Nima Parifard <paryfardnim@gmail.com>
2024-02-04 21:32:01 +0330

    stations name and line of subway information

---
+++ dataset/stations.py
@@ -0,0 +1,35 @@
+############################################# Station Name #################################
+Tajrish = 'Tajrish'
+Shariati = 'Shariati'
+Beheshti = 'Beheshti'
+Dowlat = 'Dowlat'
+Mohamadieh = 'Mohamadieh'
+Kahrizak = 'Kahrizak'
+Sanat = 'Sanat'
+Valiasr = 'Valiasr'
+Theather = 'Theather'
+Molavi = 'Molavi'
+Basij = 'Basij'
+Sattari = 'Sattari'
+Jahad = 'Jahad'
+Beheshti = 'Beheshti'
+Heravi = 'Heravi'
+Ghaem = 'Ghaem'
+Azadi = 'Azadi'
+Pherdosi = 'Pherdosi'
+Shermiran = 'Shermiran'
+Booali = 'Booali'
+############################################################################################
+
+################################  Lines of Tehran Subway ##################################
+Line1 = [Tajrish, Shariati, Beheshti,Dowlat, Mohamadieh, Kahrizak]
+Line2 = [Sanat, Valiasr, Theather, Mohamadieh, Heravi, Basij]
+Line3 = [Sattari, Valiasr, Jahad, Beheshti, Heravi, Ghaem]
+Line4 = [Azadi, Theather, Pherdosi, Dowlat, Shermiran, Booali]
+###########################################################################################
+
+
+Just_In_Station = [Tajrish, Sanat, Sattari, Azadi]
+Just_Out_Station = [Kahrizak, Basij, Ghaem, Booali]
+
+
